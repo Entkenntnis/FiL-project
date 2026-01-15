7f682c046c9c04610a31cc26c7e11d693ff48919
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-01-15 14:22:27 +0100

    continue

--- FiLProject/Tree23/RuntimeComplexity.lean
+++ FiLProject/Tree23/RuntimeComplexity.lean
@@ -6,6 +6,22 @@ universe u
 
 variable {α : Type u}
 
+
+lemma height_numNodes_log (t : Tree23 α) (ht : complete t):
+    height t + 1 ≤ Real.logb 2 (2 * numNodes t + 2) := by
+  have x := complete_height_numNodes t ht
+  have x2 := mul_le_mul_of_nonneg_left (a := 2) x (by simp)
+  have x3 := Real.logb_le_logb_of_le (b := 2) (x := 2 * 2 ^ t.height) (y := 2 * (t.numNodes + 1))
+  have t : (2 : ℝ) * 2 ^ t.height ≤ 2 * (↑t.numNodes + 1) := by norm_cast
+  specialize x3 (by simp) (by simp) t
+  rw [← pow_succ'] at x3
+  rw [Real.logb_pow] at x3
+  simp at x3
+  rw [mul_add] at x3
+  simp at x3
+  exact x3
+
+
 variable [LinearOrder α]
 
 def T_insert : α → Tree23 α → ℕ
@@ -21,21 +37,12 @@ def T_insert : α → Tree23 α → ℕ
                           (T_insert x m) + 1
                         else (T_insert x r) + 1
 
-
-lemma height_numNodes_log (t : Tree23 α) :
-    height t ≤ Nat.log 2 (numNodes t + 1) := by sorry
-
 lemma runtime_height (x : α) (t : Tree23 α) :
     T_insert x t ≤ height t + 1 := by
   induction t with
-  | nil => simp[height, T_insert]
-  | node2 l a r l_ih r_ih =>
-    unfold height
-    unfold T_insert
-    by_cases x = a
-    · sorry
-    · sorry
-  | node3 l a m b r l_ih m_ih r_ih => sorry
+  | nil => simp[T_insert]
+  | node2 l a r l_ih r_ih => grind[T_insert]
+  | node3 l a m b r l_ih m_ih r_ih => grind[T_insert]
 
 
 lemma insertion_time (x : α) (t : Tree23 α) :
