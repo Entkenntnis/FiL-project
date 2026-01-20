1228616763bd3054790eeb9860e0f155a1cc79d3
caeciliabolz <caecilia.bolz@gmx.de>
2026-01-20 15:17:20 +0100

    join

--- FiLProject/Tree23/List.lean
+++ FiLProject/Tree23/List.lean
@@ -23,3 +23,19 @@ def trees : Tree23s α → Set (Tree23 α)
 def inorder2 : Tree23s α → List α
 | T t => inorder t
 | TTs t a ts => inorder t ++ [a] ++ inorder2 ts
+
+def join_adj (t1 : Tree23 α) (a : α) (ts : Tree23s α) : Tree23s α :=
+  match ts with
+  | T t2 => T (node2 t1 a t2)
+  | TTs t2 b ts =>
+    match ts with
+    | T t3 => T (node3 t1 a t2 b t3)
+    | TTs t3 c ts => TTs (node2 t1 a t2) b (join_adj t3 c ts)
+
+def join_all : Tree23s α → Tree23 α
+| T t => t
+| TTs t a ts => join_all (join_adj t a ts)
+
+def not_T : Tree23s α → Bool
+| T _ => False
+| TTs _ _ _ => True
