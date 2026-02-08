9d12514b12ac344b10bf9b599beaefffc64b9721
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-02-08 14:01:51 +0100

    typo fixes

--- FiLProject/Tree23.lean
+++ FiLProject/Tree23.lean
@@ -3,5 +3,5 @@ import FiLProject.Tree23.Complete
 import FiLProject.Tree23.Delete
 import FiLProject.Tree23.Insert
 import FiLProject.Tree23.Search
-import FilProject.Tree23.RuntimeComplexity
-import FilProject.Tree23.List
+import FiLProject.Tree23.RuntimeComplexity
+import FiLProject.Tree23.List
