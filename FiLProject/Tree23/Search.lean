895ab5f8cd9c679f4a59f601aafa083269ab8fe0
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-02-08 12:00:17 +0100

    simplify Search

--- FiLProject/Tree23/Search.lean
+++ FiLProject/Tree23/Search.lean
@@ -8,6 +8,7 @@ variable {α : Type u}
 
 variable [LinearOrder α]
 
+@[grind]
 def isin : Tree23 α → α → Bool
 | nil,  _ => false
 | node2 l a r,  x => if x < a then (isin l x) else
@@ -35,14 +36,8 @@ def searchTree : (t : Tree23 α) → Prop
 
 lemma isin_el_setTree (t: Tree23 α) (x: α) (h: searchTree t):
     isin t x ↔ x ∈ setTree t := by
-  induction t with
-  | nil => grind[isin, setTree]
-  | node2 l a r l_ih r_ih =>  grind[isin, setTree, searchTree]
-  | node3 l a m b r l_ih m_ih r_ih => grind[isin, setTree, searchTree]
+  induction t <;> grind
 
 lemma isin_iff_setTree (t: Tree23 α) (x: α) (h: searchTree t):
     isin t x = true ↔ x ∈ setTree t := by
-  induction t with
-  | nil => grind[setTree, isin]
-  | node2 l a r l_ih r_ih => grind[setTree, isin, searchTree]
-  | node3 l a m b r l_ih m_ih r_ih => grind[setTree, isin, searchTree]
+  induction t <;> grind
